b3940ca778b1a35d8e711c139a8933b54688b047
Esdras Caleb <esdrascaleb@gmail.com>
2024-10-07 16:16:15 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import WebsmApp from './WebsmApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WebsmApp />
   </React.StrictMode>
 );
 
